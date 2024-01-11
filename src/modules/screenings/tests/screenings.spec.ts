e806389a6f79d76e366a9b6ab1cf66edfaf92be3
miglemr <migler019@gmail.com>
2024-01-11 21:26:30 +0200

    test: :test_tube: add failing test

--- src/modules/screenings/tests/screenings.spec.ts
+++ src/modules/screenings/tests/screenings.spec.ts
@@ -37,4 +37,26 @@ describe('GET', () => {
       },
     ])
   })
+
+  it('should return all screenings for provided movie id', async () => {
+    const { body } = await supertest(app).get('/screenings/133093').expect(200)
+
+    expect(body).toHaveLength(2)
+    expect(body).toEqual([
+      {
+        id: expect.any(Number),
+        date: '2024-01-20 10:00:00',
+        ticketsTotal: 20,
+        title: 'The Matrix',
+        year: 1999,
+      },
+      {
+        id: expect.any(Number),
+        date: '2024-01-28 21:30:00',
+        ticketsTotal: 15,
+        title: 'The Matrix',
+        year: 1999,
+      },
+    ])
+  })
 })
