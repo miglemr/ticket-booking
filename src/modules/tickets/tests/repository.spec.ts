fdf33f55477b370213a0bf3e8c9b229b0acc108c
miglemr <migler019@gmail.com>
2024-01-13 13:03:04 +0200

    test: :test_tube: add failing test

--- src/modules/tickets/tests/repository.spec.ts
+++ src/modules/tickets/tests/repository.spec.ts
@@ -21,4 +21,23 @@ describe('createBooking', async () => {
       id: expect.any(Number),
     })
   })
+
+  it('should add the specified number of tickets', async () => {
+    const ticket = await repository.createBooking(
+      {
+        screeningId: 1,
+      },
+      2
+    )
+
+    expect(ticket).toHaveLength(2)
+    expect(ticket).toEqual([
+      {
+        id: expect.any(Number),
+      },
+      {
+        id: expect.any(Number),
+      },
+    ])
+  })
 })
